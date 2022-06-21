98b4214ea45320184b1f1a509611de2df220c627
TortleTurtle <danielmeijs@gmail.com>
2022-06-21 17:07:32 +0200

    Marker location are generated from a fetched json file.

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,9 +1,101 @@
-import {Text, View} from "react-native";
+import {Dimensions, Text, View, StyleSheet} from "react-native";
+import MapView, {Marker} from "react-native-maps";
+import {useEffect, useState} from "react";
+
 
 export default function MapScreen(){
+    const [hotspots, setHotspots] = useState([]);
+
+    useEffect(() => {   fetchHotspots() }, []);
+
+    const fetchHotspots = async () => {
+        console.log("Fetching hotspots");
+        try {
+            const res = await fetch("https://stud.hosted.hr.nl/0965152/websites/LiBrewRy/hotspots.json");
+            const data = await res.json();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    const loadMarkers = () => {
+        console.log("loading markers")
+        const markers = [];
+        //loop through all hotspots
+        hotspots.forEach((hotspot) => {
+        //then loop through all venues
+            hotspot.venues.forEach((venue, i) => {
+                 markers.push(<Marker key={venue.venue} title={venue.venue} coordinate={venue.latLong}/>);
+            });
+        });
+        console.log(markers[0]);
+        return markers
+    }
+
     return(
-        <View>
-            <Text>This is the map view</Text>
+        <View style={styles.container}>
+            <MapView style={styles.map}
+                     initialRegion={{
+                latitude: 51.915925469994704,
+                longitude: 4.477762127342756,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            }}
+                     showsPointsOfInterest={false}
+                     //Hide POI for Android.
+                     customMapStyle={[
+                 {
+                     "featureType": "administrative.land_parcel",
+                     "elementType": "labels",
+                     "stylers": [
+                         {
+                             "visibility": "off"
+                         }
+                     ]
+                 },
+                 {
+                     "featureType": "poi.business",
+                     "stylers": [
+                         {
+                             "visibility": "off"
+                         }
+                     ]
+                 },
+                 {
+                     "featureType": "poi.park",
+                     "elementType": "labels.text",
+                     "stylers": [
+                         {
+                             "visibility": "off"
+                         }
+                     ]
+                 },
+                 {
+                     "featureType": "road.local",
+                     "elementType": "labels",
+                     "stylers": [
+                         {
+                             "visibility": "off"
+                         }
+                     ]
+                 }
+             ]}
+            >
+                {/*TODO: On marker click go to a detail screen*/}
+                {loadMarkers().map((marker) => { return marker})}
+            </MapView>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
